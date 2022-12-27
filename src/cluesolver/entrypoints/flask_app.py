de2c769d9af96545fc023d8d24083812433d097b
David Breese <dabreese00@gmail.com>
2022-12-26 20:32:13 -0500

    Add game detail API endpoint

--- src/cluesolver/entrypoints/flask_app.py
+++ src/cluesolver/entrypoints/flask_app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import NoResultFound
+from markupsafe import escape
 
 import cluesolver.config as config
 import cluesolver.domain.cluegame as cluegame
@@ -14,10 +16,21 @@ app = Flask(__name__)
 
 
 @app.route("/games", methods=["POST"])
-def games_endpoint():
+def create_game():
     session = get_session()
     repo = repository.SqlAlchemyRepository(session)
     game = cluegame.Game(request.json["name"])
     repo.add(game)
     session.commit()
     return {"name": game.name}, 201
+
+
+@app.route("/games/<game_name>", methods=["GET"])
+def get_game(game_name):
+    session = get_session()
+    repo = repository.SqlAlchemyRepository(session)
+    try:
+        game = repo.get(escape(game_name))
+    except NoResultFound as e:
+        return {"message": str(e)}, 404
+    return {"name": game.name}
--- tests/e2e/test_api.py
+++ tests/e2e/test_api.py
@@ -1,5 +1,6 @@
 import uuid
 import requests
+import pytest
 
 from cluesolver.config import get_api_url
 
@@ -8,59 +9,85 @@ def random_name():
     return str(uuid.uuid1())
 
 
-def test_api_returns_game(postgres_session):
+@pytest.mark.usefixtures('postgres_session')
+def test_api_post_returns_game():
     game_name = random_name()
     url = get_api_url()
 
     r = requests.post(f"{url}/games", json={'name': game_name})
 
-    [[name]] = postgres_session.execute(
-        "SELECT name FROM game"
-    )
-
     assert r.status_code == 201
     assert r.json()['name'] == game_name
+
+
+def test_api_post_game_is_saved(postgres_session):
+    game_name = random_name()
+    url = get_api_url()
+
+    requests.post(f"{url}/games", json={'name': game_name})
+
+    [[name]] = postgres_session.execute(
+        "SELECT name FROM game WHERE name=:name", {'name': game_name}
+    )
+
     assert name == game_name
 
-# @pytest.fixture
-# def add_game(postgres_session):
-#     games_added = set()
-#     players_added = set()
-#     cards_added = set()
-#
-#     def _add_game(game_name, player_name, card_name):
-#         postgres_session.execute(
-#             "INSERT INTO game (name) VALUES (:name)", {'name': game_name}
-#         )
-#         [[game_id]] = postgres_session.execute(
-#             "SELECT id FROM game WHERE name = :name", {'name': game_name}
-#         )
-#         postgres_session.execute(
-#             "INSERT INTO player (name, game_id, hand_size) "
-#             "VALUES (:name, :game_id, :hand_size)",
-#             {'name': player_name, 'game_id': game_id, 'hand_size': 3}
-#         )
-#         postgres_session.execute(
-#             "INSERT INTO card (name, game_id, card_type) "
-#             "VALUES (:name, :game_id, :card_type)",
-#             {'name': card_name, 'game_id': game_id, 'card_type': 'Room'}
-#         )
-#         games_added.add(game_name)
-#         players_added.add(player_name)
-#         cards_added.add(card_name)
-#         postgres_session.commit()
-#
-#     yield _add_game
-#
-#     for game_name in games_added:
-#         postgres_session.execute(
-#             "DELETE FROM game WHERE name = :name", {'name': game_name}
-#         )
-#     for player_name in players_added:
-#         postgres_session.execute(
-#             "DELETE FROM player WHERE name = :name", {'name': player_name}
-#         )
-#     for card_name in cards_added:
-#         postgres_session.execute(
-#             "DELETE FROM card WHERE name = :name", {'name': card_name}
-#         )
+
+@pytest.mark.xfail
+def test_api_get_returns_games(postgres_session):
+    game_name = random_name()
+    game_name_two = random_name()
+    url = get_api_url()
+
+    postgres_session.execute(
+        "INSERT INTO game (name) VALUES (:name)", {'name': game_name}
+    )
+    postgres_session.execute(
+        "INSERT INTO game (name) VALUES (:name)", {'name': game_name_two}
+    )
+
+    r = requests.get(f"{url}/games")
+
+    names = [entry['name'] for entry in r.json()]
+
+    assert r.status_code == 200
+    assert game_name in names
+    assert game_name_two in names
+
+
+def test_api_returns_single_game(postgres_session):
+    game_name = "abc"
+    url = get_api_url()
+
+    postgres_session.execute(
+        "INSERT INTO game (name) VALUES (:name)", {'name': game_name}
+    )
+    postgres_session.commit()
+
+    r = requests.get(f"{url}/games/{game_name}")
+
+    assert r.status_code == 200
+    assert r.json()['name'] == game_name
+
+
+@pytest.mark.xfail
+def test_api_deletes_game(postgres_session):
+    game_name = random_name()
+    url = get_api_url()
+
+    postgres_session.execute(
+        "INSERT INTO game (name) VALUES (:name)", {'name': game_name}
+    )
+    [[game_id]] = postgres_session.execute(
+        "SELECT id FROM game WHERE name = :name", {'name': game_name}
+    )
+
+    r = requests.delete(f"{url}/games/{game_id}")
+
+    assert r.status_code == 204
+
+    games = postgres_session.execute(
+        "SELECT id FROM game WHERE name = :name", {'name': game_name}
+    )
+
+    assert len(games) == 0
